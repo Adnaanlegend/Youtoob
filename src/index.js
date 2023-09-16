4e8628d2d54b2af5940cfea6a027aaec54683cbd
Adnaan Hussain <adnaan137@gmail.com>
2023-09-16 18:40:04 +0530

    Phase 1 done

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM from "react-dom/client"; //Changed in version 18 of React
+ 
+import App from "./App";
+import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
